0f73ffcd988de93a964a7fd569a1d511792d716b
shahzad akram <alyakram786@gmail.com>
2024-01-24 00:20:01 +0500

    another function working

--- index.js
+++ index.js
@@ -19,10 +19,34 @@ btnC.addEventListener('click', () => {
 })
 
 
-let num = 1
-let numS = 10;
-for (let i = 0; i < num.length; i++) {
-    for (let j = 0; j < numS.length; i++) {
-        console.log(num)
+// let num = 1
+// let numS = 10;
+// for (let i = 0; i < num.length; i++) {
+//     for (let j = 0; j < numS.length; i++) {
+//         console.log(num)
+//     }
+// }
+
+// let num2 = 3;
+// let range = 5;
+// for (let i = 0; i < num2.length; i++) {
+//     for (let j = 0; j <= range.length; j++) {
+//         if (true) {
+//             console.log("here is the answer:", i + 1, i + 2, i + 3, i + 4, i + 5)
+//         }
+
+//     }
+// }
+// console.log(num2)
+
+
+let num2 = 3;
+let range = 5;
+
+for (let i = 0; i < num2; i++) {
+    let sum = 0;
+    for (let j = 0; j <= range; j++) {
+        sum += i + 1 + i + 2 + i + 3;
     }
+    console.log("Sum for i =", i, "is:", sum);
 }
