fff1d77db5486b8c280e056c73f45a7be31264d0
shahzad akram <alyakram786@gmail.com>
2024-01-23 19:24:10 +0500

    adding new repo

---
+++ index.js
@@ -0,0 +1,19 @@
+let btnC = document.getElementById("buttncalc");
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2")
+let para = document.getElementById("prgrph")
+
+
+
+btnC.addEventListener('click', () => {
+    let inputVlaues = [
+        parseFloat(input1.value),
+        parseFloat(input2.value)
+    ]
+    for (let i = 0; i < inputVlaues.length; i++) {
+        for (let j = 0; j < inputVlaues.length; j++) {
+
+        }
+
+    }
+})
