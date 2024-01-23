463ac62867c274144118f4fa6ce3edc56202afd1
shahzad akram <alyakram786@gmail.com>
2024-01-24 02:14:25 +0500

    almost completed ulat pulat task

--- index.js
+++ index.js
@@ -38,24 +38,53 @@ let U_para = document.getElementById("ulatpara");
 
 
 
-
-
-
-
-
-
-let str = "hi";
-for (let i = 0; i <= str.length; i++) {
-    for (let j = 0; j <= str.length; j++) {
-        let str2 = str[j];
-        // str = str[1, 0]
-        str = str[1] + str[0];
-
+U_butn.addEventListener('click', () => {
+    let inputT2 = U_input1.value;
+    let tempT2 = "";
+    for (let i = inputT2.length - 1; i >= 0; i--) {
+        tempT2 += inputT2[i]
 
     }
+    U_para.innerHTML = tempT2;
 
-}
-console.log(str)
+
+    // for (let i = 0; i < inputT2.length; i++) {
+    //     for (letj = 0; j <= inputT2.length; j++) {
+    //         let tempT2 = inputT2[j];
+    //         inputT2 = inputT2[4] + inputT2[3] + inputT2[2] + inputT2[1] + inputT2[0];
+
+    //     }
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+// let str = "hello";
+// for (let i = 0; i <= str.length; i++) {
+//     for (let j = 0; j <= str.length; j++) {
+//         let str2 = str[j];
+//         // str = str[1, 0]
+//         str = str[4] + str[3] + str[2] + str[1] + str[0];
+
+
+//     }
+
+// }
+// console.log(str)
+
+
+
+
+
 
 // let num = 1
 // let numS = 10;
