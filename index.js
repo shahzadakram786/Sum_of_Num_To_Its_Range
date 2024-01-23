ab0a73fc39e521d9867d99e97da835ca212640c9
shahzad akram <alyakram786@gmail.com>
2024-01-23 23:58:25 +0500

    another function

--- index.js
+++ index.js
@@ -10,13 +10,19 @@ btnC.addEventListener('click', () => {
         parseFloat(input1.value),
         parseFloat(input2.value)
     ]
-    for (let i = 0; i < inputVlaues.length; i++) {
-        for (let j = 0; j < inputVlaues.length; j++) {
-            if () {
-
-            }
-
+    for (let i = 0; i < input1.length; i++) {
+        for (let j = 0; j < input2.length; j++) {
+            console.log()
         }
 
     }
 })
+
+
+let num = 1
+let numS = 10;
+for (let i = 0; i < num.length; i++) {
+    for (let j = 0; j < numS.length; i++) {
+        console.log(num)
+    }
+}
